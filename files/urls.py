fb85ae19cadc46f785b9759d07476580d9d6f84c
Virgin Bitton <virgin.bitton@gmail.com>
2022-02-04 21:43:23 +0100

    renommage des route de partage d'accès pour + de clareté et standardisation

--- files/urls.py
+++ files/urls.py
@@ -15,9 +15,9 @@ urlpatterns = [
     path('files/', ListCreateFiles.as_view(), name="listcreatefiles"),
     path('files/<int:pk>', RetrieveUpdateDestroyFile.as_view(), name="retrieveupdatedestroyfile"),
     path('files/<int:pk>/download', DownloadFile.as_view(), name="downloadfile"),
-    path('files/share/<int:pk>', ShareFile.as_view(), name="sharefile"),
+    path('files/<int:pk>/share', ShareFile.as_view(), name="sharefile"),
     path('folders/', ListCreateFolders.as_view(), name="listcreatefolders"),
     path('folders/<int:pk>', RetrieveUpdateDestroyFolder.as_view(), name="retrieveupdatedestroyfolder"),
     path('folders/<int:pk>/content', RetrieveContentInFolder.as_view(), name="retrievefilesinfolder"),
-    path('folders/share/<int:pk>', ShareFolder.as_view(), name="sharefolder"),
+    path('folders/<int:pk>/share', ShareFolder.as_view(), name="sharefolder"),
 ]
