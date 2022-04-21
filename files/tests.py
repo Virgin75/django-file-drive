982ca689113906c419289f64483923e32541a80f
Virgin Bitton <virgin.bitton@gmail.com>
2022-04-21 22:28:03 +0200

    ajout de tous les tests sur l'objet Folder

--- files/tests.py
+++ files/tests.py
@@ -3,29 +3,8 @@ import json
 from django.contrib.auth import get_user_model
 from files.models import Folder, File
 from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
 
-
-
-@pytest.fixture
-def auth_client(db):
-    client = APIClient()
-    usr = client.post(
-        '/signup',
-        {'email': 'virgin225@gmail.com',
-        'password': 'Azerty123$',
-        'first_name': 'Virgin',
-        'last_name': 'Bitton'},
-    )
-    login = client.post(
-        '/api/token/',
-        {'email': 'virgin225@gmail.com',
-        'password': 'Azerty123$'},
-    )
-    resp_json = json.loads(login.content)
-    print(resp_json)
-    access_token = resp_json['access']
-    client.credentials(HTTP_AUTHORIZATION='Bearer ' + access_token)
-    return client
 
 @pytest.fixture
 def user(db):
@@ -35,21 +14,29 @@ def user(db):
     )
 
 @pytest.fixture
+def other_user(db):
+    return get_user_model().objects.create(
+        email='laurent336@gmail.com',
+        password='Azerty123$'
+    )
+
+
+@pytest.fixture
+def auth_client(db, user):
+    client = APIClient()
+    token = RefreshToken.for_user(user)
+    client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(token.access_token))
+    return client
+
+
+@pytest.fixture
 def root_folder(db, user):
-    return Folder.objects.create(
+    folder = Folder.objects.create(
         folder_name='Test Folder', 
         color='#022063',
         owner=user)
-
-
-
-# Test retrieve users's folders list
-@pytest.mark.django_db
-def test_retrieve_my_folders(auth_client):
-    response = auth_client.get('/api/folders/')
-    resp_json = json.loads(response.content)
-    assert response.status_code == 200
-    assert len(resp_json) >= 1
+    folder.save()
+    return folder
 
 
 # Test create a folder
@@ -65,6 +52,16 @@ def test_create_folder(root_folder, auth_client):
     assert response.status_code == 201
     assert resp_json['id']
 
+
+# Test retrieve a folder's details
+@pytest.mark.django_db
+def test_retrieve_folder_details(auth_client, root_folder):
+    response = auth_client.get('/api/folders/' + str(root_folder.id))
+    resp_json = json.loads(response.content)
+    assert response.status_code == 200
+    assert resp_json['id'] == root_folder.id
+
+
 # Test update a folder
 @pytest.mark.django_db
 def test_update_folder(root_folder, auth_client):
@@ -74,3 +71,23 @@ def test_update_folder(root_folder, auth_client):
     resp_json = json.loads(response.content)
     assert response.status_code == 200
     assert resp_json['folder_name'] == 'New name'
+
+
+# Test share a folder with another user
+@pytest.mark.django_db
+def test_share_folder(other_user, root_folder, auth_client):
+    endpoint = f'/api/folders/{root_folder.id}/share'
+    response = auth_client.post(endpoint, {
+        'email': other_user.email,
+    })
+    resp_json = json.loads(response.content)
+    assert response.status_code == 201
+    assert resp_json['folder']['id'] == root_folder.id
+
+
+# Test delete a folder
+@pytest.mark.django_db
+def test_delete_folder(other_user, root_folder, auth_client):
+    endpoint = f'/api/folders/{root_folder.id}'
+    response = auth_client.delete(endpoint)
+    assert response.status_code == 204
